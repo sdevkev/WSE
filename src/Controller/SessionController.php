d63f758cd7f996f4f79deb9df2a455ddd78e82bc
sdevkev <kevin-dunne@hotmail.com>
2020-04-03 14:27:43 +0100

    adding orderdetails functionality

---
+++ src/Controller/SessionController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Controller;
+
+
+// imports
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class SessionController extends AbstractController
+{
+		/**
+	* @Route("/addToCart") methods("GET", "POST");
+	*/
+	public function AddToCart()
+	{
+		
+		
+		
+	}
+	 
+	 
+}
